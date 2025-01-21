956e66023e8676096fb789971ad03b5b5bce3957
Aaryan Pashine <pashineaaryan@gmail.com>
2025-01-21 16:11:04 -0500

    cool i guess?

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 import fs from "fs"
+import { auth } from "./auth.js"
 
-// get 
+// get channel
+let channel = await fetch("https://api.are.na/v2/channels/blog-feed",
+	{
+		headers: {
+			Authorization: `Bearer ${auth}`,
+			cache: "no-store",
+			"Cache-Control": "max-age=0, no-cache",
+			referrerPolicy: "no-referrer",
+		},
+
+	}
+)
+	.then((res) => res.json())
+
+console.log(channel)
